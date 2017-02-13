98e54612826b3fac24e7689a967aec7e82e9359d
thenameisnat <talbot.nat@gmail.com>
2017-02-13 18:49:03 -0500

    Add new file change:

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+//Compile with git -o HelloWorld HelloWorld.cpp
+//Run with ./HelloWorld from the command line
+
+#include <iostream>
+
+int main()
+{
+std::cout << "Hello, World!\n";
+std::cout << "Git is the best!\n";
+
+}
